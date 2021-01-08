786d2779bf4c103126fde207a2f3beb4b6ef0894
Th3Wall <mandellidavide.95@gmail.com>
2021-01-08 03:03:37 +0100

    course checkpoint

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from 'react';
+import './App.sass';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      monsters: []
+    }
+  }
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(user => this.setState({ monsters: user }))
+  }
+
+  render () {
+    return (
+      <div className='App'>
+        {
+          this.state.monsters.map(el => <h2 key={el.id}>{el.name}</h2>)
+        }
+      </div>
+    )
+  }
 }
 
 export default App;
