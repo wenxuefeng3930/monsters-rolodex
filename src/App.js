7077dfd5443adda95d7977aaa3ed8a3750740679
Th3Wall <mandellidavide.95@gmail.com>
2021-01-11 16:38:05 +0100

    UI inserted

--- src/App.js
+++ src/App.js
@@ -26,6 +26,7 @@ class App extends Component {
 
     return (
       <div className='App'>
+        <h1>Monsters Rolodex</h1>
         <SearchBox
           placeholder={'Search monsters'}
           handleChange={(e) => this.setState({searchField: e.target.value})}
--- src/components/card-list/card-list.jsx
+++ src/components/card-list/card-list.jsx
@@ -3,13 +3,23 @@ import './card-list.sass';
 
 export const CardList = props => {
     return (
-        <div className='card-list'>
-            {props.monsters.map(monster =>
-                <Card
-                    key={monster.id}
-                    monster={monster}
-                />
-            )}
-        </div>
+        <>
+            {
+                props.monsters.length === 0 ? (
+                    <div className='card-notfound'>
+                        <h2>☹️ No monster found</h2>
+                    </div>
+                ) : (
+                    <div className='card-list'>
+                        {props.monsters.map(monster =>
+                            <Card
+                                key={monster.id}
+                                monster={monster}
+                            />
+                        )}
+                    </div>
+                )
+            } 
+        </>
     )
 }
